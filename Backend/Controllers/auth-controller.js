0ede43268e88a3595ccb0f2fe4f5eafacd61aeb6
saiteja527 <boddepalli.ramana48@gmail.com>
2024-08-06 16:45:36 +0530

    "save"

--- Backend/Controllers/auth-controller.js
+++ Backend/Controllers/auth-controller.js
@@ -1,65 +1,61 @@
 const User = require("../models/user-model");
-const Book = require("../models/users-other")
+const Book = require("../models/users-other");
 const Driver = require("../models/drivers");
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET_KEY
+const secret = process.env.JWT_SECRET_KEY;
+
 const home = async (req, res) => {
     try {
         res.status(200).json({ message: "WELCOME ROUTER" });
-    } catch (err) {
+    } catch (error) {
         console.log(error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
 };
 
 const register = async (req, res, next) => {
     try {
-        console.log(req.body);
         const { username, password, email, phone } = req.body;
-
         const userExist = await User.findOne({ email });
         if (userExist) {
-            return res.status(400).json({ msg: "User already exists" });
+            return res.status(400).json({ message: "User already exists" });
         }
-        const userCreated = await User.create({ username, password, email, phone });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const userCreated = await User.create({ username, password: hashedPassword, email, phone });
 
         res.status(201).json({
-            msg: "registration successful",
+            message: "Registration successful",
             userCreated
-            // token: await userCreated.generateToken(),
-            // userId: userCreated._id.toString(),
         });
     } catch (error) {
-        //res.status(500).send("internal server error");
         next(error);
     }
-}
+};
+
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
         const userExist = await User.findOne({ email });
-
-        console.log(userExist);
         if (!userExist) {
             return res.status(400).json({ message: "Invalid credentials" });
         }
-        //const user = await userExist.comparePassword(password);
-        if (userExist.password !== password) {
-            return res.status(400).json({ message: "Invalid password" })
+
+        const isPasswordValid = await bcrypt.compare(password, userExist.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid password" });
         }
 
-
-        const token = { "userId": userExist._id, "username": userExist.username, "email": userExist.email, "phone": userExist.phone }
-        return res.status(200).json({ message: "Successfully logged in", token })
-
-
+        const token = jwt.sign({ userId: userExist._id, username: userExist.username, email: userExist.email, phone: userExist.phone }, secret, { expiresIn: '1h' });
+        return res.status(200).json({ message: "Successfully logged in", token });
     } catch (error) {
-        res.status(500).json("internal server error");
+        res.status(500).json({ message: "Internal server error" });
     }
-}
+};
+
 const book = async (req, res, next) => {
     try {
-        //console.log(req.body);
         const { username, phone, clientId, ride, date, source, destination } = req.body;
         const rideCreated = await Book.create({ username, phone, clientId, ride, date, source, destination });
         res.status(201).json({
@@ -70,30 +66,20 @@ const book = async (req, res, next) => {
         res.status(500).send("internal server error");
         next(error);
     }
-}
+};
 
 const bookDetails = async (req, res, next) => {
     try {
         const details = await Book.find().sort({ date: "asc" });
-        //if(ride === false){
-        //   return res.status(400).json({msg:"Ride is not booked"});
-        //}
-
-        console.log(details);
         res.status(201).json({ msg: "Booking details", details });
     } catch (error) {
-        //res.status(500).send("internal server error");
         next(error);
     }
-}
+};
 
 const driver = async (req, res, next) => {
     try {
-        //console.log(req.body);
         const { name, license, model, phone, age } = req.body;
-        //if(ride === false){
-        //   return res.status(400).json({msg:"Ride is not booked"});
-        //}
         const driverCreated = await Driver.create({ name, license, model, phone, age });
         res.status(201).json({
             msg: "Driver details registered successfully",
@@ -103,35 +89,37 @@ const driver = async (req, res, next) => {
         res.status(500).send("internal server error");
         next(error);
     }
-}
+};
 
 const acceptRide = async (req, res, next) => {
     try {
-
         const { id, driverName, driverPhone } = req.params;
-        console.log(id);
+        console.log("Ride ID:", id);
+        console.log("Driver Name:", driverName);
+        console.log("Driver Phone:", driverPhone);
 
-        const updatedRideStatus = await Book.findByIdAndUpdate(id, { ride: true, driverName: driverName, driverPhone: driverPhone })
-        return res.status(201).json({ message: "ride accepted successfully", updatedRideStatus })
-
+        const updatedRideStatus = await Book.findByIdAndUpdate(id, { ride: true, driverName: driverName, driverPhone: driverPhone }, { new: true });
+        if (!updatedRideStatus) {
+            return res.status(404).json({ message: "Ride not found" });
+        }
+        return res.status(200).json({ message: "Ride accepted successfully", updatedRideStatus });
     } catch (error) {
-        res.status(500).send("internal server error");
+        console.error(error);
+        res.status(500).send("Internal Server Error");
         next(error);
     }
-}
+};
+
 
 const userTripsHistory = async (req, res, next) => {
     try {
-
         const { clientId } = req.params;
         const history = await Book.find({ clientId });
-        return res.status(201).json({ msg: "history retrieved succesfully", history })
-        console.log(history)
-
+        return res.status(201).json({ msg: "history retrieved successfully", history });
     } catch (error) {
         res.status(500).send("internal server error");
         next(error);
     }
-}
+};
 
-module.exports = { home, register, login, book, bookDetails, driver, acceptRide, userTripsHistory }
+module.exports = { home, register, login, book, bookDetails, driver, acceptRide, userTripsHistory };
